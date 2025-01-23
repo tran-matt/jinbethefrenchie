09096ef9752d51d1bb11b80000de92dfba859d4a
tran-matt <143528178+tran-matt@users.noreply.github.com>
2025-01-23 11:44:56 -0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>Welcome to Frenchie's World!</h1>
+      </header>
+      <main>
+        <section>
+          <h2>About My Frenchie</h2>
+          <p>This is where I'll showcase my adorable French Bulldog!</p>
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
