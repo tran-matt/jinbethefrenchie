076faef9ebb753483a31e652167ae5222e9b21ec
tran-matt <143528178+tran-matt@users.noreply.github.com>
2025-02-07 15:49:58 -0500

    style changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import HeroSection from "../components/HeroSection";
 
-// Sample blog posts for the homepage preview
 const blogPosts = [
   {
     id: 1,
